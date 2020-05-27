b7aea86cfe5eeb06151f381d6b35b04ec048070b
AlenYing <rachel120000@126.com>
2020-05-27 23:54:46 +0800

    添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,12 +1,16 @@
 <template>
-    <button class="g-button">
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
         <slot></slot>
     </button>
 </template>
 
 <script>
-  export default {
-  }
+    export default {
+        props: ['icon', 'iconPosition']
+    }
 </script>
 
 <style lang="scss">
@@ -17,14 +21,26 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-    &:hover {
-        border-color: var(--border-color-hover);
-    }
-    &:active {
-        background-color: var(--button-active-bg);
-    }
-    &:focus {
-        outline: none;
-    }
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
+            }
+        }
     }
 </style>
